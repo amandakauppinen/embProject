b515f43042a5d1245f81ce30284859e5f3e6c8fd
Amanda Kauppinen <amandakauppinen3@gmail.com>
2019-03-13 16:53:04 +0200

    Sorry, the stuff from main didn't get pushed in the last one so it should be there now

--- src/Project_ventilation.cpp
+++ src/Project_ventilation.cpp
@@ -17,10 +17,35 @@
 #endif
 
 #include <cr_section_macros.h>
+#include <ritimer_15xx.h>
+#include "LiquidCrystal.h"
+#include <atomic>
+#include "UI.h"
 
-// TODO: insert other include files here
+static volatile std::atomic_int counter;
 
-// TODO: insert other definitions and declarations here
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+//* @brief Handle interrupt from SysTick timer
+//* @return Nothing
+
+void SysTick_Handler(void)
+{
+	if(counter > 0) counter--;
+}
+#ifdef __cplusplus
+}
+#endif
+void Sleep(int ms)
+{
+	counter = ms;
+	while(counter > 0) {
+			__WFI();
+	}
+}
+
 
 int main(void) {
 
@@ -32,11 +57,45 @@ int main(void) {
     // functions related to the board hardware
     Board_Init();
     // Set the LED to the state of "On"
-    Board_LED_Set(0, true);
+    uint32_t sysTickRate;
+
+	Chip_Clock_SetSysTickClockDiv(1);
+	sysTickRate = Chip_Clock_GetSysTickClockRate();
+	SysTick_Config(sysTickRate / 1000);
+
+    Chip_RIT_Init(LPC_RITIMER);
+
+    //Board_LED_Set(0, true);
 #endif
 #endif
 
     // TODO: insert code here
+
+    DigitalIoPin rs(0, 8, false); //RS
+	DigitalIoPin en(1, 6, false); //EN
+	DigitalIoPin d4(1, 8, false); //D4
+	DigitalIoPin d5(0, 5, false); //D5
+	DigitalIoPin d6(0, 6, false); //D6
+	DigitalIoPin d7(0, 7, false); //D7
+
+	DigitalIoPin b1(1,3,true, true, true);
+	DigitalIoPin b2(0,9,true, true, true);
+	DigitalIoPin b3(0,10,true, true, true);
+
+
+	LiquidCrystal lcd(&rs, &en, &d4, &d5, &d6, &d7);
+
+	lcd.begin(16, 2);
+	lcd.clear();
+
+
+	UI menu(&lcd, &b1, &b2, &b3);
+
+	while (1) {
+		menu.buttonStatus();
+	}
+
+
 
     // Force the counter to be placed into memory
     volatile static int i = 0 ;
