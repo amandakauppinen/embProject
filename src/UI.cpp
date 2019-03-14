cc04568b7067bd46f616261ec40fdbd67384f390
Quang Vu <33469486+shirmplord@users.noreply.github.com>
2019-03-14 19:46:07 +0200

    UI update, finishing project layout
    Error code still not working @Fabian

--- src/UI.cpp
+++ src/UI.cpp
@@ -1,15 +1,15 @@
 #include "UI.h"
 
-
 /*
  * MANUAL = TRUE		AUTO = FALSE
  */
 
 UI::UI(LiquidCrystal *screen, DigitalIoPin *b1, DigitalIoPin *b2, DigitalIoPin *b3) {
 	this->screen = screen;
-	this->b1 = b1; //+
-	this->b2 = b2; //-
-	this->b3 = b3; //Select
+	this->b1 = b1;
+	this->b2 = b2;
+	this->b3 = b3;
+	manual = false;
 	//sets it to automatically start in manual mode
 	displayFormat();
 }
@@ -19,143 +19,236 @@ void UI::clearDisplay() {
 }
 
 void UI::buttonStatus() {
-	//this might be better going into the main
-	if (b1->read()) {
-		//if the device is in manual mode
-		if (manualMode == true) {
-			if (fanSpeed <= 95) {
-				fanSpeed += 5;
-			}
-			else {
-				fanSpeed = 100;
-			}
-			//update fan speed, send it to lcd
-			//Update the LCD readings
-			displayFormat();
-		}
-		//if the device is in auto mode
-		else if (manualMode == false) {
-			if (pressure <= 115) {
-				pressure += 5;
-			}
-			else {
-				pressure = 120;
-			}
-			//update pressure value, send it to lcd
+	//update values of pressure and fan speed every time button status is checked
+//	getPressure();
+//	getFan();
 
-			//Update the LCD readings
-			displayFormat();
-		}
-		while(b1->read()) {}
-	}
-	else if (b2->read()) {
-		if (manualMode == true) {
-			if (fanSpeed >= 5) {
-				fanSpeed -= 5;
+	//this might be better going into the main
+//	while(1) {
+		if (b1->read()) {
+//			Sleep(500);
+			//if the device is in manual mode
+			if (manual) {
+				if (fanSpeed <= 95) {
+					fanSpeed += 5;
+				}
+				else {
+					fanSpeed = 100;
+				}
+				//update fan speed, send it to lcd
+//				setFan(fanSpeed);
+				valueUpdate();
 			}
-			else {
-				fanSpeed = 0;
+			//if the device is in auto mode
+			else if (!manual) {
+				if (pressure <= 115) {
+					pressure += 5;
+				}
+				else {
+					pressure = 120;
+				}
+				//update pressure value, send it to lcd
+//				setPressure(pressure);
+				valueUpdate();
 			}
-			//Update the LCD
-			displayFormat();
 		}
-		else if (manualMode == false) {
-			if (pressure >=5) {
-				pressure -= 5;
+		else if (b2->read()) {
+//			Sleep(500);
+			if (manual == true) {
+				if (fanSpeed >= 5) {
+					fanSpeed -= 5;
+				}
+				else {
+					fanSpeed = 0;
+				}
+//				setFan(fanSpeed);
+				valueUpdate();
 			}
-			else {
-				pressure = 0;
+			else if (manual == false) {
+				if (pressure >=5) {
+					pressure -= 5;
+				}
+				else {
+					pressure = 0;
+				}
+//				setPressure(pressure);
+				valueUpdate();
 			}
-			//Update the LCD
+		}
+		else if (b3->read()) {
+//			Sleep(500);
+			//I can make this look nicer later
+			//if device is in manual mode, change to auto
+			if (manual == true) {
+				manual = false;
+			}
+			else if (manual == false){
+				manual = true;
+			}
 			displayFormat();
 		}
-		while(b2->read()) {}
-	}
-	else if (b3->read()) {
-		screen->clear();
-		if (manualMode == true) {
-			manualMode = false;
-			displayFormat();
-		}
-		else if (manualMode == false){
-			manualMode = true;
-			displayFormat();
-		}
-		while(b3->read()) {}
-	}
+//	}
 }
+
 
 
 /*This section is a mess right now until I can figure out the problem with those random characters
  * it works, but it's too much code and messy*/
 
 void UI::displayFormat () {
-	char buffer[16] = {'\0'};
-	int cx;
+	//get most recent pressure and speed values
+//	getPressure();
+//	getFan();
+
+	//clear display to start new
+	clearDisplay();
 
 	//change lcd to manual layout
-	if (manualMode == true) {
+	if (manual) {
 		//line 0
 		screen->setCursor(0,0);
-		cx = snprintf ( buffer, 16, "Pressure: %d", pressure);
-		screen->print(buffer);
+		screen->print("Pressure: ");
+		screen->print(" ");
+		screen->setCursor(10,0);
+		screen->print(std::to_string(pressure));
+
+		//This is the part to take care of random characters
+		if (pressure >= 100) {
+			screen->setCursor(13,0);
+		}
+		else if (pressure < 10) {
+			screen->setCursor(11,0);
+			screen->print(" ");
+		}
+		else {
+			screen->setCursor(12,0);
+		}
+		screen->print(" ");
+
+
 		screen->setCursor(14,0);
 		screen->print("pa");
-		buffer[0] = {'\0'};
-
 
 		//line 1
 		screen->setCursor(0,1);
-		cx = snprintf(buffer, 16, "Set speed: %d", fanSpeed);
-		screen->print(buffer);
+		screen->print("Set speed: ");
+		screen->setCursor(11,1);
+		screen->print(std::to_string(fanSpeed));
+
+		//This is the part to take care of random characters
+		if (fanSpeed >= 100) {
+			screen->setCursor(14,1);
+		}
+		else if (fanSpeed < 10) {
+			screen->setCursor(12,1);
+			screen->print(" ");
+		}
+		else {
+			screen->setCursor(13,1);
+		}
+		screen->print(" ");
+
+
 		screen->setCursor(15,1);
 		screen->print("%");
-		buffer['\0'];
 	}
 	//change device to auto layout
 	else {
-		//line 0
-		screen->setCursor(0,0);
-		cx = snprintf(buffer, 16, "Pressure: %d", currentPressure);
-		screen->print(buffer);
-		screen->setCursor(14,0);
-		screen->print("pa");
-		buffer['\0'];
 		//line 1
 		screen->setCursor(0,1);
-		cx = snprintf(buffer, 16, "Set pr: %d", pressure);
-		screen->print(buffer);
+		screen->print("Set pr: ");
+		screen->setCursor(8,1);
+		screen->print(std::to_string(pressure));
+
+		//This is the part to take care of random characters
+		if (pressure >= 100) {
+			screen->setCursor(11,1);
+		}
+		else if (pressure < 10) {
+			screen->setCursor(9,1);
+			screen->print(" ");
+		}
+		else {
+			screen->setCursor(10,1);
+		}
+		screen->print(" ");
+
+
 		screen->setCursor(12,1);
 		screen->print("pa");
-		buffer['\0'];
 	}
 }
 
+//updates values on lcd as they're changed
+void UI::valueUpdate () {
+	//if in manual mode
+	if (manual) {
+		//I don't know if this is necessary to keep
+//		getPressure();
+		screen->setCursor(10,0);
+		screen->print(std::to_string(pressure));
+
+		screen->setCursor(11,1);
+		screen->print(std::to_string(pressure));
+
+		//This is the part to take care of random characters
+		if (pressure >= 100) {
+			screen->setCursor(14,1);
+		}
+		else if (pressure < 10) {
+			screen->setCursor(12,1);
+			screen->print(" ");
+		}
+		else {
+			screen->setCursor(13,1);
+		}
+		screen->print(" ");
+	}
+	else {
+		screen->setCursor(8,1);
+
+		//This is the part to take care of random characters
+		screen->print(std::to_string(pressure));
+		if (pressure >= 100) {
+			screen->setCursor(11,1);
+		}
+		else if (pressure < 10) {
+			screen->setCursor(9,1);
+			screen->print(" ");
+		}
+		else {
+			screen->setCursor(10,1);
+		}
+		screen->print(" ");
+	}
+}
 
 //set fan and pressure speeds
-//void UI::setFan(int newSpeed){
-//	this->fanSpeed = newSpeed;
-//	displayFormat();
-//}
-void UI::setPressure(int newPressure) {
-	this->pressure = newPressure;
-	displayFormat();
+/*we need to link these to the main program, I'll also try to add in a way
+to check if the value being sent is already set to be that, in which case
+it will not have to do anything different*/
+void UI::setFan(int fanSpeed){
+	this->fanSpeed = fanSpeed;
+}
+void UI::setPressure(int pressure) {
+	this->pressure = pressure;
 }
 
 
 //gets fan and pressure speeds to be displayed
 //needs to be updated to match rest of program
 int UI::getFan() {
-	return (fanSpeed*200);
+	//fanSpeed = 100;
+	return fanSpeed;
 }
 int UI::getPressure() {
+	//pressure = 100;
 	return pressure;
 }
 
 //leaving this alone for now, we can call it from wherever we are checking the error
 void UI::errorMessage() {
-	screen->setCursor(0,0);
-	screen->print("                ");
+	screen->clear();
 	screen->setCursor(0,0);
 	screen->print("Error code: 123");
 }
--- src/UI.h
+++ src/UI.h
@@ -1,42 +1,38 @@
 #ifndef UI_h
+
 #define UI_h
 
-#include "DigitalIoPin.h"
+
 #include "LiquidCrystal.h"
+
 #include <string>
 
+
 class UI {
+
 public:
+	
 	UI(LiquidCrystal *screen, DigitalIoPin *b1, DigitalIoPin *b2, DigitalIoPin *b3);
 	void clearDisplay();
 	void buttonStatus();
-
-	//void setFan(int newSpeed);
-	void setPressure(int newPressure);
-	void inline setCurrentPressure (int pa) {
-		currentPressure = pa;
-	}
-
+	void setFan(int fanSpeed);
+	void setPressure(int pressure);
 	void errorMessage();
-
 	int getFan();
 	int getPressure();
-	bool inline Mode() {
-		return manualMode;
-	}
+	bool inline getMode() {return manual;};
 	virtual ~UI();
 private:
 	LiquidCrystal *screen;
 	DigitalIoPin *b1;
 	DigitalIoPin *b2;
 	DigitalIoPin *b3;
-	
 	//true = manual, false = auto
-	bool manualMode = true;
-	int pressure = 90, fanSpeed = 10;
-	int currentPressure;
+	bool manual = true, error = false;
+	int pressure = 0, fanSpeed = 0;
 
+	void valueUpdate ();
 	void displayFormat ();
-};
 
+};
 #endif
